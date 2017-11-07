fa6a5d79b92255fa51445463b532744e28854d52
vrto <victor.tripeno@gft.com>
2017-11-07 13:36:22 -0200

    Ajuste rota

--- camel/src/main/java/com/palestra/camel/routes/RotaPrincipal.java
+++ camel/src/main/java/com/palestra/camel/routes/RotaPrincipal.java
@@ -10,17 +10,88 @@ public class RotaPrincipal extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+    	
+    	errorHandler(
+				deadLetterChannel("direct:error")
+				.logExhausted(true)
+		);
+    	
+    	from("direct:error")
+    		.setBody(constant("ERROR"))
+			.setHeader(Exchange.HTTP_RESPONSE_CODE, constant("500"))
+    	.end();
+    	
+    	
         from("direct:distribuidor")
             .multicast(new AggregationStrategy() {
                 @Override
                 public Exchange aggregate(Exchange exchange, Exchange exchange1) {
-                	if(exchange1 != null) {
+                	if(exchange != null) {
                 		
                 	}
-                    return exchange;
+                    return exchange1;
                 }
             }, true)
             .to("","")
             .end();
     }
 }
+
+/*.multicast().parallelProcessing()
+.toD("direct:chamada1")
+//.toD("direct:chamada2")
+//.toD("direct:chamada3")
+//.toD("direct:chamada1")
+//.toD("direct:chamada2")
+//.toD("direct:chamada3")
+.end()
+.to("log:custom")
+.log("${body}");
+//
+
+from("direct:qualquer")
+.setProperty("meuId", constant("1"))
+.setBody(constant("VALOR CHAMADA QUAlQUER 1"))
+.toD("direct:agregador")
+.end();
+
+from("direct:agregador")
+.aggregate(property("meuId"),(oldExchange, newExchange) -> {
+System.out.println("ENTROU AGG");
+if(oldExchange != null) {
+	System.out.println("OLD " + oldExchange.getIn().getBody(String.class));
+	System.out.println("VALOR CONCATENADO: old = " + oldExchange.getIn().getBody(String.class) + " new = " + newExchange.getIn().getBody(String.class));
+}
+if(newExchange != null) {
+	System.out.println("NEW " + newExchange.getIn().getBody(String.class));
+}
+return newExchange;
+}).eagerCheckCompletion().completionSize(2)
+.to("log:foo")
+.end();
+
+from("direct:chamada1")
+.setProperty("meuId", constant("1"))
+.setBody(constant("${body}1"))
+.to("socket-tcp://SOCKET:16010")
+.setBody(constant("VALOR CHAMADA 1"))
+.toD("direct:agregador")
+.end();
+
+from("direct:chamada2")
+.setProperty("meuId", constant("2"))
+.setBody(constant("${body}2"))
+.to("socket-tcp://SOCKET:16010")
+.setBody(constant("VALOR CHAMADA 2"))
+.toD("direct:agregador")
+.end();
+
+from("direct:chamada3")
+.setProperty("meuId", constant("3"))
+.setBody(constant("${body}3"))
+.to("socket-tcp://SOCKET:16010")
+.setBody(constant("VALOR CHAMADA 3"))
+.toD("direct:agregador")
+.end();
+}*/
+
